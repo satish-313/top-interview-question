a2e892f5ddddb24650b64478dce8debe2b983baf
satish-313 <pradhansatish53@gmail.com>
2023-01-22 21:30:56 +0530

    5 longest palidrome substring

--- javascript/5-longest-palindromic-substring.js
+++ javascript/5-longest-palindromic-substring.js
@@ -25,15 +25,40 @@ const longestPalindromeBt = function (s) {
   return output;
 };
 
-console.log(longestPalindromeBt("babad")); // O(n^3)
-console.log(longestPalindromeBt("cbbd")); // O(n^3)
+// console.log(longestPalindromeBt("babad")); // O(n^3)
+// console.log(longestPalindromeBt("cbbd")); // O(n^3)
 
+function longestPalindrome(s) {
+  let bestLen = 0;
+  let bestStr = s[0];
+  let n = s.length;
 
-/* 
-    b a b a d
-  b t f t 
-  a 
-  b
-  a
-  d
-*/
+  for (let mid = 0; mid < n; mid++) {
+    for (let x = 0; mid - x >= 0 && mid + x < n; x++) {
+      if (s[mid - x] !== s[mid + x]) break;
+      let len = 2 * x + 1;
+      if (bestLen < len) {
+        bestLen = len;
+        bestStr = s.substr(mid - x, len);
+      }
+    }
+  }
+
+  for (let mid = 0; mid < n - 1; mid++) {
+    for (let x = 1; mid - x + 1 >= 0 && mid + x < n; x++) {
+      if (s[mid - x + 1] !== s[mid + x]) break;
+      let len = 2 * x;
+
+      if (bestLen < len) {
+        bestLen = len;
+        bestStr = s.substr(mid - x + 1,len);
+      }
+    }
+  }
+
+  return bestStr;
+}
+
+console.log(longestPalindrome("babad"));
+console.log(longestPalindrome("cbbd"));
+console.log(longestPalindrome("ccd"))
